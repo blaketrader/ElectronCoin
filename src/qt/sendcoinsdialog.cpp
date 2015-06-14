97f49c835e17dd4c5210e13d7a700c8de49987d0
cinnamon_carter <cinnamoncoin@users.noreply.github.com>
2015-06-14 14:27:26 +0000

    add valid sample elt address

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -33,7 +33,7 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent) :
 #endif
 #if QT_VERSION >= 0x040700
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a Bitcoin address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
+    ui->lineEditCoinControlChange->setPlaceholderText(tr("Enter a Bitcoin address (e.g. e3PZPuuckSmvqWSNGtPBp3vysZ8pegkwUu)"));
 #endif
 
     addEntry();
